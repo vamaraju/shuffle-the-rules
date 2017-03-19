907704e20e778e5831d57ef6ef04eccf6e9f507c
theWhyteRabbit <svidal@ualberta.ca>
2017-03-18 19:18:47 -0600

    adding model classes related to Actions and Events. So far added: GameAction, DrawCard

---
+++ ShuffleTheRules/src/model/DrawCard.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kirsten on 2017-03-18.
+ */
+public class DrawCard implements GameAction {
+    @Override
+    public void run(ArrayList<GameAction> actions) {
+
+    }
+}
