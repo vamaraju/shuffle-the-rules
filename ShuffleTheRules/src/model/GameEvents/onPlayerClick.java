dcda88290512104d4f2f1bbd5644b6fa65ba3f93
theWhyteRabbit <svidal@ualberta.ca>
2017-03-18 20:01:23 -0600

    Game Event interface and classes that implement it (on class UML) added

---
+++ ShuffleTheRules/src/model/GameEvents/onPlayerClick.java
@@ -0,0 +1,15 @@
+package model.GameEvents;
+
+import model.GameActions.GameAction;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kirsten on 2017-03-18.
+ */
+public class onPlayerClick implements GameEvent {
+    @Override
+    public void run(ArrayList<GameAction> actions) {
+
+    }
+}
