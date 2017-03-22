2a44bbdbea9440adc9fbd9042237749243cf42d0
theWhyteRabbit <svidal@ualberta.ca>
2017-03-22 15:49:41 -0600

    Player class get/set. classes added:BasicPile

---
+++ ShuffleTheRules/src/model/BasicPile.java
@@ -0,0 +1,18 @@
+package model;
+
+
+public class BasicPile extends Pile {
+
+    public BasicPile(String name, int minSize, int maxSize) {
+        super(name, minSize, maxSize);
+    }
+
+    public void view(){
+
+    }
+
+    public void swap(Pile pileToSwapWith, int cardIndexCurrentPile, int cardIndexSwapPile ){
+
+    }
+
+}
--- ShuffleTheRules/src/model/Player.java
+++ ShuffleTheRules/src/model/Player.java
@@ -5,4 +5,40 @@ public class Player {
     private int playerID;
     private boolean status;
     private int turnOrder;
+
+    public Player(){
+
+    }
+
+    public int getHandId() {
+        return handId;
+    }
+
+    public void setHandId(int handId) {
+        this.handId = handId;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getTurnOrder() {
+        return turnOrder;
+    }
+
+    public void setTurnOrder(int turnOrder) {
+        this.turnOrder = turnOrder;
+    }
 }
