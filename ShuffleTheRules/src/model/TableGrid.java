60ff0d537213b931a3417dea63f5f866b03065f5
theWhyteRabbit <svidal@ualberta.ca>
2017-03-22 16:31:32 -0600

    added TableGrid. will populate once GUI creation starts

---
+++ ShuffleTheRules/src/model/TableGrid.java
@@ -0,0 +1,15 @@
+package model;
+
+
+import java.util.ArrayList;
+
+public class TableGrid {
+    int gridDimensionX;
+    int getGridDimensionY;
+    float gridScaleFactor;
+    Boolean showGridlines;
+    ArrayList gridValues;
+    public TableGrid(){
+
+    }
+}
