c0f6969511412f6436cee1d0e3c95a0f30ebe76d
theWhyteRabbit <svidal@ualberta.ca>
2017-03-25 21:26:50 -0600

    CardSettings class methods created.

--- ShuffleTheRules/src/model/CardSettings.java
+++ ShuffleTheRules/src/model/CardSettings.java
@@ -1,10 +1,11 @@
 package model;
 
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+/* TODO how will Joker be handled?*/
+/* TODO Error handling for methods */
 public class CardSettings {
     /* stores all Card information:
        <value:<heart: num_hearts
@@ -18,16 +19,33 @@ public class CardSettings {
     }
 
     private void initialize(){
-
+        String intialDeck[] = {"Ace","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+        for (String card: intialDeck) {
+            /* Default values - deck with 52 cards */
+            updateAllSuits(card, 1, 1, 1, 1);
+        }
     }
 
     public Set<String> getCardList(){
         return cardSettings.keySet();
     }
 
-    public void updateSuit(String key, String suit, int value){
-        
+    public void updateSuit(String card, String suit, int value){
+        cardSettings.get(card).put(suit,value);
     }
 
+    public void updateAllSuits(String card, int heartValue, int spadeValue, int clubValue, int diamondValue){
+        updateSuit(card, "heart", heartValue);
+        updateSuit(card, "spade", spadeValue);
+        updateSuit(card, "club", clubValue);
+        updateSuit(card, "diamond", diamondValue);
+    }
 
+    public int getSuitCount(String card, String suit){
+        return cardSettings.get(card).get(suit);
+    }
+
+    public int getTotalSuitCount(String card){
+        return getSuitCount(card,"heart") +  getSuitCount(card,"spade") + getSuitCount(card,"club") +  getSuitCount(card,"diamond");
+    }
 }
