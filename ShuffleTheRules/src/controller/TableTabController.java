e87e4d8d399a99c2b6500d166d226d31caaf213b
theWhyteRabbit <svidal@ualberta.ca>
2017-03-26 21:46:11 -0600

    starting work on TableTabView Controller

--- ShuffleTheRules/src/controller/TableTabController.java
+++ ShuffleTheRules/src/controller/TableTabController.java
@@ -1,9 +1,19 @@
 package controller;
 
 
+import model.CardSettings;
+import model.TableGrid;
+import view.TableTab.TableTabView;
+
 public class TableTabController {
+    private CardSettings cardSettings;
+    private TableGrid tableGrid;
+    private TableTabView tableTabView;
 
-    public TableTabController(){
 
+    public TableTabController(CardSettings cardSettingsModel, TableGrid tableGridModel, TableTabView tableTabView){
+        cardSettings = cardSettingsModel;
+        tableGrid = tableGridModel;
+        tableTabView = tableTabView;
     }
 }
