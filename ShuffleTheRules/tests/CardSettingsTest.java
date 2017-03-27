0bd11dbf5eac6781e0a61af86f0bb08664d11164
theWhyteRabbit <svidal@ualberta.ca>
2017-03-26 21:17:30 -0600

    added file for JUnit tests, which CardSettingsTest.java. please use JUnit4

---
+++ ShuffleTheRules/tests/CardSettingsTest.java
@@ -0,0 +1,61 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CardSettingsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getCardList() throws Exception {
+
+    }
+
+    @Test
+    public void updateSuit() throws Exception {
+
+    }
+
+    @Test
+    public void updateAllSuits() throws Exception {
+
+    }
+
+    @Test
+    public void getSuitCount() throws Exception {
+
+    }
+
+    @Test
+    public void getTotalSuitCount() throws Exception {
+
+    }
+
+    @Test
+    public void clearAllCardSettings() throws Exception {
+
+    }
+
+    @Test
+    public void removeCard() throws Exception {
+
+    }
+
+    @Test
+    public void addCard() throws Exception {
+
+    }
+
+}
