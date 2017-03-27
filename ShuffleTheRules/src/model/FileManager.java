a31f3ffd99f5598ba22a86ad557cb1c5b25c99a1
theWhyteRabbit <svidal@ualberta.ca>
2017-03-26 21:41:00 -0600

    added Serializer class. updated FileManager (minor update to be consistent with Serializer)

--- ShuffleTheRules/src/model/FileManager.java
+++ ShuffleTheRules/src/model/FileManager.java
@@ -3,5 +3,17 @@ package model;
 
 public class FileManager {
 
-    
+    String filepath;
+
+    public GameCreation loadFromFile(String filepath){
+        return new GameCreation();
+    }
+
+    public void saveToFile(GameCreation gameData){
+
+    }
+
+    public void autosave(GameCreation gameData){
+
+    }
 }
---
+++ ShuffleTheRules/src/model/Serializer.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Serializer {
+
+    public void serialize(GameCreation gameData){
+
+    }
+
+    public GameCreation deserialize(){
+        return new GameCreation();
+    }
+}
