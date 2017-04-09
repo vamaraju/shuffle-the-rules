d17dbd2f9da77b43d693e1329d47c183095472f8
theWhyteRabbit <svidal@ualberta.ca>
2017-04-08 23:22:16 -0600

    starting work on Gameplay window

---
+++ ShuffleTheRules/src/view/Gameplay/GameplayView.java
@@ -0,0 +1,16 @@
+package view.Gameplay;
+
+import javafx.scene.layout.BorderPane;
+
+
+public class GameplayView extends BorderPane {
+
+    public GameplayView(){
+        initialize();
+    }
+
+    public void initialize(){
+
+    }
+
+}
