8f031ee2767121a0d2dc32aeb7d32cef016a6943
theWhyteRabbit <svidal@ualberta.ca>
2017-04-11 21:28:40 -0600

    commit so can pull

--- ShuffleTheRules/src/view/TableTab/TableGridView.java
+++ ShuffleTheRules/src/view/TableTab/TableGridView.java
@@ -1,11 +1,7 @@
 package view.TableTab;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+
 
 
 public class TableGridView extends GridPane {
