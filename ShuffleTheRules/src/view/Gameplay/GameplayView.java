3dc15d91807a1fddcda0762d0087dd2ad343ba7f
theWhyteRabbit <svidal@ualberta.ca>
2017-04-13 18:36:53 -0600

    originally created MenuBar view and controller for Gameplay mode. decided it was not needed (wrt requirements). have end game button instead

--- ShuffleTheRules/src/view/Gameplay/GameplayView.java
+++ ShuffleTheRules/src/view/Gameplay/GameplayView.java
@@ -1,14 +1,11 @@
 package view.Gameplay;
 
-import controller.GameplayController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import model.GameplayMessage;
 import view.TableTab.TableGridView;
 
 
@@ -18,7 +15,7 @@ public class GameplayView extends BorderPane {
 
     private Button endTurnButton = new Button("End Turn");
     private Button sortHandButton = new Button("Sort Hand");
-
+    private Button endGameButton = new Button("End Game");
 
     private GameplayMessageView gameplayMessageView;
 
@@ -28,7 +25,6 @@ public class GameplayView extends BorderPane {
 
     public void initialize(){
 
-        /* top will be the menu bar */
 
         /* centre pane will be the table */
         /* need to pass in TableGrid values*/
@@ -50,6 +46,7 @@ public class GameplayView extends BorderPane {
         GridPane buttonsPane = new GridPane();
         buttonsPane.add(endTurnButton,2,1,2,2);
         buttonsPane.add(sortHandButton,2,3,2,2);
+        buttonsPane.add(endGameButton,2,5,2,2);
 
         /* buttons will be disabled unless condition satisified */
         endTurnButton.setDisable(true);
@@ -65,6 +62,10 @@ public class GameplayView extends BorderPane {
 
     public Button getSortHandButton() {
         return sortHandButton;
+    }
+
+    public Button getEndGameButton() {
+        return endGameButton;
     }
 
     public GameplayMessageView getGameplayMessageView(){
