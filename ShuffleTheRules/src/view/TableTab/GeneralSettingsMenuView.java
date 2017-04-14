de473123f2dffbbda6194d96760939736f892538
vamaraju <vamaraju@ualberta.ca>
2017-04-14 14:55:32 -0600

    Added player settings to GeneralMenuView

--- ShuffleTheRules/src/view/TableTab/GeneralSettingsMenuView.java
+++ ShuffleTheRules/src/view/TableTab/GeneralSettingsMenuView.java
@@ -8,6 +8,7 @@ import controller.TableTab.GeneralSettingsMenuController;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import model.Player;
 import model.TripleHashMap;
 
 import javax.xml.soap.Text;
@@ -36,13 +37,31 @@ public class GeneralSettingsMenuView extends TitledPane {
         generalMenuContent.setHgap(2);
         generalMenuContent.setVgap(4);
 
-        elements.put("playersHeader", new Label("Number of Players"), null);
+        elements.put("numPlayersHeader", new Label("Number of Players"), null);
         elements.put("minPlayers", new Label("Min:"), new TextField());
         elements.put("maxPlayers", new Label("Max:"), new TextField());
+
+        elements.put("playerSettingsHeader", new Label("Player Settings"), null);
+        elements.put("player", new Label("Player:"), new ComboBox<Player>());
+        elements.put("playerName", new Label("Player Name:"), new TextField());
+        elements.put("playerTurn", new Label("Turn Number:"), new ComboBox<Integer>());
+
         elements.put("handSizeHeader", new Label("Hand Size"), null);
         elements.put("minHandSize", new Label("Min:"), new TextField());
         elements.put("maxHandSize", new Label("Max:"), new TextField());
         elements.put("startingHandSize", new Label("Starting:"), new TextField());
+
+        String[] headers = {"numPlayersHeader", "playerSettingsHeader", "handSizeHeader"};
+        for (int i = 0; i < headers.length; i++) {
+            elements.getValue1(headers[i]).setStyle("-fx-font-weight: bold");
+        }
+
+        ((TextField) elements.getValue2("minPlayers")).setPromptText("Enter a Number");
+        ((TextField) elements.getValue2("maxPlayers")).setPromptText("Enter a Number");
+        ((TextField) elements.getValue2("playerName")).setPromptText("Enter a Name");
+        ((TextField) elements.getValue2("minHandSize")).setPromptText("Enter a Number");
+        ((TextField) elements.getValue2("maxHandSize")).setPromptText("Enter a Number");
+        ((TextField) elements.getValue2("startingHandSize")).setPromptText("Enter a Number");
 
         addGridContent();
 
@@ -59,12 +78,8 @@ public class GeneralSettingsMenuView extends TitledPane {
                 }
                 generalMenuContent.add(elements.getValue1(key), 0, row++);
             } else {
-                Node n = elements.getValue2(key);
-                if (n instanceof TextField) {
-                    ((TextField) n).setMaxSize(50, 20);
-                }
                 generalMenuContent.add(elements.getValue1(key), 0, row);
-                generalMenuContent.add(n, 1, row++);
+                generalMenuContent.add(elements.getValue2(key), 1, row++);
             }
         }
         generalMenuContent.add(updateButton, 0, row++);
@@ -74,11 +89,67 @@ public class GeneralSettingsMenuView extends TitledPane {
         return updateButton;
     }
 
+    public TextField getMinPlayersTextField() {
+        return (TextField) elements.getValue2("minPlayers");
+    }
+
     public String getMinPlayersTextFieldValue() {
-        return ((TextField) elements.getValue2("minPlayers")).getText();
+        return getMinPlayersTextField().getText();
+    }
+
+    public TextField getMaxPlayersTextField() {
+        return (TextField) elements.getValue2("maxPlayers");
     }
 
     public String getMaxPlayersTextFieldValue() {
-        return ((TextField) elements.getValue2("maxPlayers")).getText();
+        return getMaxPlayersTextField().getText();
+    }
+
+    public ComboBox getPlayerComboBox() {
+        return (ComboBox) elements.getValue2("player");
+    }
+
+    public Player getPlayerComboBoxValue() {
+        return (Player) getPlayerComboBox().getValue();
+    }
+
+    public TextField getPlayerNameTextField() {
+        return (TextField) elements.getValue2("playerName");
+    }
+
+    public String getPlayerNameTextFieldValue() {
+        return getPlayerNameTextField().getText();
+    }
+
+    public ComboBox getTurnNumberComboBox() {
+        return (ComboBox) elements.getValue2("playerTurn");
+    }
+
+    public Integer getTurnNumberComboBoxValue() {
+        return (Integer) getTurnNumberComboBox().getValue();
+    }
+
+    public TextField getMinHandSizeTextField() {
+        return (TextField) elements.getValue2("minHandSize");
+    }
+
+    public String getMinHandSizeTextFieldValue() {
+        return getMinHandSizeTextField().getText();
+    }
+
+    public TextField getMaxHandSizeTextField() {
+        return (TextField) elements.getValue2("maxHandSize");
+    }
+
+    public String getMaxHandSizeTextFieldValue() {
+        return getMaxHandSizeTextField().getText();
+    }
+
+    public TextField getStartingHandSizeTextField() {
+        return (TextField) elements.getValue2("startingHandSize");
+    }
+
+    public String getStartingHandSizeTextFieldValue() {
+        return getStartingHandSizeTextField().getText();
     }
 }
