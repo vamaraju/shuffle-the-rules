dccece917898373d2b015c3abc2b89d28a667965
theWhyteRabbit <svidal@ualberta.ca>
2017-04-15 21:51:07 -0600

    selecting was working the whole time. use control click to select multiple or deselect.... will clean up files and add message for user

--- ShuffleTheRules/src/model/CardCell.java
+++ ShuffleTheRules/src/model/CardCell.java
@@ -13,27 +13,11 @@ import javafx.scene.input.MouseEvent;
 public class CardCell extends ListCell<Card> {
 
 
-    boolean  clicked;
-
     public CardCell(){
-        this.setOnMouseClicked(this::cellClicked);
-        clicked = false;
+
     }
 
-    private void cellClicked(MouseEvent mouseEvent) {
-        System.out.println("cell originally is " + this.isSelected());
-        clicked = !clicked;
-        if(clicked){
 
-            this.setStyle("-fx-background-color: blue;");
-            System.out.println("cell deselected" + this.getItem().getSuit() + " value " + this.getItem().getValue());
-
-        }else{
-
-            this.setStyle("-fx-background-color: white;");
-            System.out.println("cell selected" + this.getItem().getSuit() + " value " + this.getItem().getValue());
-        }
-    }
 
     @Override
     public void updateItem(Card item, boolean empty) {
@@ -41,11 +25,11 @@ public class CardCell extends ListCell<Card> {
 
         if (item != null) {
             ImageView cardImage;
-            //if (item.getOrientation() == CardOrientation.UP ) {
+            if (item.getOrientation() == CardOrientation.UP ) {
                 cardImage = new ImageView(new Image(item.getCardFaceAssetLocation()));
-            //}else{
-            //    cardImage = new ImageView(new Image(item.getCardBackAssetLocation()));
-            //}
+            }else{
+                cardImage = new ImageView(new Image(item.getCardBackAssetLocation()));
+            }
             cardImage.setPreserveRatio(true);
             cardImage.setFitWidth(150);
             cardImage.setFitHeight(350);
