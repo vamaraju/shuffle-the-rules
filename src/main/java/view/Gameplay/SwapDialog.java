2f2cd093c4f55f50a23e95eb34040c0f063a3536
theWhyteRabbit <svidal@ualberta.ca>
2017-05-04 16:32:23 -0600

    initial SwapDialog layout complete. Not a fan of how it looks, but it will be ok for now

--- src/main/java/view/Gameplay/SwapDialog.java
+++ src/main/java/view/Gameplay/SwapDialog.java
@@ -11,6 +11,8 @@ import javafx.beans.value.ObservableValue;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import model.Piles.Pile;
 
 /*
@@ -23,11 +25,13 @@ import model.Piles.Pile;
 public class SwapDialog extends Dialog<ButtonType> {
 
     private GridPane dialogLayout = new GridPane();
+    private VBox pile1Layout = new VBox();
+    private VBox pile2Layout = new VBox();
 
     private ChoiceBox pile1Selection;
     private ChoiceBox pile2Selection;
 
-    private PileView pile1_PileView;
+    private PileView pile1_pileView;
     private PileView pile2_pileView;
 
     public SwapDialog(){
@@ -47,16 +51,25 @@ public class SwapDialog extends Dialog<ButtonType> {
     }
 
     private void initLayout() {
+
+        dialogLayout.setVgap(10);
+        dialogLayout.setHgap(4);
+
         /* VERY IMPORTANT WARNING MESSAGE */
         Label pileWarningMessage = new Label("Control click to select and deselect multiple cards");
         pileWarningMessage.setStyle("-fx-font-weight: bold");
 
-        dialogLayout.add(pileWarningMessage,1,1);
+        dialogLayout.add(pileWarningMessage,1,1,6,1);
 
+        dialogLayout.add(pile1Selection,1,2,2,1);
+        dialogLayout.add(pile2Selection,3,2,2,1);
+
+        dialogLayout.add(pile1_pileView,1,4,2,4);
+        dialogLayout.add(pile2_pileView,3,4,2,4);
     }
 
     private void initPileViews(){
-        pile1_PileView = new PileView(Orientation.VERTICAL);
+        pile1_pileView = new PileView(Orientation.VERTICAL);
         pile2_pileView = new PileView(Orientation.VERTICAL);
     }
 
