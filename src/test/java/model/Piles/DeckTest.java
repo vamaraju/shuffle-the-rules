57a470f6ea066a72baf3881f120ea1020041a4ba
theWhyteRabbit <svidal@ualberta.ca>
2017-05-04 16:15:26 -0600

    added DeckTest file with tests to be filled in (will do asap)

---
+++ src/test/java/model/Piles/DeckTest.java
@@ -0,0 +1,35 @@
+package model.Piles;
+
+import model.Card;
+import model.CardOrientation;
+import model.PlayingCard;
+import model.Suit;
+import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class DeckTest {
+
+    /* Because Deck extends Pile, and all of the Pile constructors have already been tested inside PileTest,
+     * Deck constructors will not be tested. */
+
+    @Test
+    public void testDealHandToPlayer() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testDealNumCardsToPlayer() {
+        assertTrue(false);
+
+    }
+
+    @Test
+    public void testDealCardsToPlayers(){
+        assertTrue(false);
+
+    }
+}
