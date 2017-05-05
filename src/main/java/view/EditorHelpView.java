c66bb5fbea2a4f25f93221e08be63ec57a9e6ec7
vamaraju <vamaraju@ualberta.ca>
2017-05-05 10:09:47 -0600

    Minor edits to EditorHelp messages

--- src/main/java/view/EditorHelpView.java
+++ src/main/java/view/EditorHelpView.java
@@ -23,21 +23,21 @@ public class EditorHelpView extends TextFlow {
                 "OnRoundStartEvent with the appropriate player. It does this automatically. There should be no " +
                 "game rules following OnTurnEndEvent; it will automatically call the " +
                 "OnRoundStartEvent/OnTurnStartEvent. If there are any game rules immediately proceeding " +
-                "OnTurnEndEvent, they will launch BEFORE updating the player to the next player, and before launching" +
+                "OnTurnEndEvent, they will launch BEFORE updating the player to the next player, and before launching " +
                 "OnRoundStartEvent or OnTurnStartEvent.\n\n");
         Text text6 = new Text("---If OnTurnEndEvent is on the same level as any other game rule, it will be launched last.\n\n");
         Text text7 = new Text("---PlayerWinAction is expected to follow the \"win condition\", which is typically " +
                 "an event like OnHandEmptyEvent. It indicates that the player has won the game, and will " +
-                "essentially end the game; any game rules the launch after this point may or may not work correctly\n\n");
+                "essentially end the game; any game rules the launch after this point may or may not work correctly.\n\n");
         Text text8 = new Text("---If actions and events are in the same level in the graph (i.e., they have the " +
-                "same previous rule, or immediately follow the same game rule), all of the actions will always run" +
+                "same previous rule, or immediately follow the same game rule), all of the actions will always run " +
                 "first, and then the events.\n\n");
-        Text text9 = new Text("Multiple actions can follow the same game rule. During gameplay, if an action cannot " +
+        Text text9 = new Text("---Multiple actions can follow the same game rule. During gameplay, if an action cannot " +
                 "be taken (due to any restrictions placed on the action), the user can click the \"Skip Action\" " +
                 "button, and the next action in the same level will be attempted.\n\n");
-        Text text10 = new Text("Once an event or action is successfully taken, no other events or actions in the " +
+        Text text10 = new Text("---Once an event or action is successfully taken, no other events or actions in the " +
                 "same level will be attempted. The graph will proceed depth-first through the successful rule, and " +
-                "ignore (it will not run) any other rules at the same depth level.");
+                "ignore (it will not run) any other rules at the same depth level.\n\n");
 
         this.getChildren().addAll(title, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10);
     }
