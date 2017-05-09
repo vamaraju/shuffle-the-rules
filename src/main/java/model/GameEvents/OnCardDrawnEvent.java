7d7a8b10dcc1f894fb6e141a7eb68eade8196aef
theWhyteRabbit <svidal@ualberta.ca>
2017-05-08 18:28:40 -0600

    func reqs added

--- src/main/java/model/GameEvents/OnCardDrawnEvent.java
+++ src/main/java/model/GameEvents/OnCardDrawnEvent.java
@@ -1,3 +1,12 @@
+/*
+* Requirements mandating inclusion:
+*
+* 3.2.1.6.3.2 User can create Game Rules by selecting Game Events and adding a Game Action to that event.
+* 3.2.1.3.7.2 User can view Event actions and rules.
+* 3.2.1.3.7.3 User can edit Event actions and rules.
+* 3.2.2.5.3.1 Rule Interpreter will interpret and execute Game Rules for a Card Game.
+* 3.2.1.6.3.1 User can specify Game Rules associated with particular Cards.
+* */
 package model.GameEvents;
 
 import model.Card;
